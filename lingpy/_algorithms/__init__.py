ccc1f1e68fb94efd80c720cde4f16cfb11e0897c
Johann-Mattis List <mattis.list@gmail.com>
2012-11-06 21:18:18 +0100

    some first trials with wordlist-class

--- lingpy/_algorithms/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-This module provides basic algorithms for sequence comparison, sequence
-modeling, and sequence clustering.
-"""
-from align import *
-from cluster import *
-from misc import *
-
