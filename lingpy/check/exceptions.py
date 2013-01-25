7d0942009fdd4a93f50cfade207844330993a4f5
LinguList <mattis.list@posteo.de>
2013-01-25 09:52:31 +0100

    added exceptions to check-module

---
+++ lingpy/check/exceptions.py
@@ -0,0 +1,23 @@
+# created: 2013-01-25
+# modified: 2013-01-25
+"""
+Module provides common warnings for LingPy.
+"""
+__author__ = "Johann-Mattis List"
+__date__ = "2013-01-25"
+
+class ThirdPartyModuleError(Exception):
+    """
+    Common exception to be raised if third party modules are not installed.
+    """
+    def __init__(self,value):
+        self.value = "Import of third party module <{0}> failed.".format(value)
+        self.value += " Some methods won't work."
+
+    def __str__(self):
+        return repr(self.value)
+
+    def warning(self):
+
+        return "[WARNING] "+self.value
+
