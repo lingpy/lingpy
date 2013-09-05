b29e7ee66c993afce34515e45014d0cc5ba1e30c
LinguList <mattis.list@posteo.de>
2013-09-05 18:48:19 +0200

    added new module for evaluation of automatic linguistic reconstruction (ALR, alr.py)

--- lingpy/evaluate/__init__.py
+++ lingpy/evaluate/__init__.py
@@ -1,14 +1,15 @@
 # author   : Johann-Mattis List
 # email    : mattis.list@gmail.com
 # created  : 2013-04-11 07:50
-# modified : 2013-04-11 07:50
+# modified : 2013-09-05 18:35
 """
 Basic module for the evaluation of algorithms.
 """
 
 __author__="Johann-Mattis List"
-__date__="2013-04-11"
+__date__="2013-09-05"
 
 
 from .acd import *
 from .apa import *
+from .alr import *
