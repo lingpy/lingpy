35fdedb7fbe83f906206d50971e8162341d1ece2
LinguList <mattis.list@posteo.de>
2013-11-02 11:13:15 +0100

    nothing really to commit, just having problems with my git

--- scripts/orthography/test_orthography.py
+++ scripts/orthography/test_orthography.py
@@ -1,9 +1,6 @@
-# author   : Steven Moran                                                                                                         
-# email    : steve.moran@lmu.de                                                                                                   
-# created  : 2013-01-10                                                                                                           
-
+# -*- coding: utf-8 -*-
 """
-Test script for the orthography module.
+Test script for the orthography / tokenization module.
 """
 
 __author__ = "Steven Moran"
@@ -11,14 +8,18 @@ __date__ = "2010-12-01"
 
 from lingpy import *
 
-o = OrthographyParser("../../lingpy/data/orthography_profiles/test.prf")
-g = GraphemeParser()
-test_words = ["aa", "aabuu", "uuabaa auubaa"]
+word = "Màttís List"
+
+t = Tokenizer("../../lingpy/data/orthography_profiles/test.prf", "../../lingpy/data/orthography_profiles/test.rules")
+
 print()
-for word in test_words:
-    print("original word:", word)
-    print("parse_string_to_graphemes_string:", o.parse_string_to_graphemes_string(word))
-    print("parse_string_to_ipa_string:", o.parse_string_to_ipa_string(word))
-    print("parse_string_to_graphemes:", o.parse_string_to_graphemes(word))
-    # print("parse_graphemes:", g.parse_graphemes(word))                                                                      
-    print()
+print(word+"\t\t\t"+"word")
+print(t.characters(word)+"\t\t"+"Tokenizer.characters(word)")
+print(t.grapheme_clusters(word)+"\t\t"+"Tokenizer.grapheme_clusters(word)")
+print(t.graphemes(word)+"\t\t"+"Tokenizer.graphemes(word)")
+print(t.transform(word)+"\t\t"+"Tokenizer.transform(word)")
+print(t.transform(word, "ipa")+"\t\t"+"Tokenizer.transform(word, 'ipa')")
+print(t.transform(word, "funny")+"\t\t"+"Tokenizer.transform(word, 'funny')")
+print(t.rules(word)+"\t\t\t\t"+"Tokenizer.rules(word)")
+
+
