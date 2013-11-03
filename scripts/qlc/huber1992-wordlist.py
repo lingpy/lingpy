3f87f5a0e0bf3e965f19b930cab3df804c20427b
bambooforest <bambooforest@gmail.com>
2013-11-03 01:36:51 +0100

    scripts to load and tokenize qlc data

---
+++ scripts/qlc/huber1992-wordlist.py
@@ -0,0 +1,21 @@
+"""
+Example script that loads and tokenizes QLC data into a LingPy dictionary.
+
+1. Load Huber 1992 comparative wordlist data from QLC format into a LingPy wordlist
+2. Tokenize the data given the .prf and .rules files in data/orthography_profiles
+3. Write the tokenized data to disk in QLC format.
+
+"""
+
+__author__ = "Steven Moran"
+__date__ = "2013-11-01"
+
+from lingpy import *
+
+file = read_qlc("../../lingpy/data/qlc/huber1992.csv")
+wl = Wordlist(file)
+wl.tokenize("../../lingpy/data/orthography_profiles/huber1992.prf", "../../lingpy/data/orthography_profiles/huber1992.rules")
+wl.output("qlc", filename="huber1992-tokenized")
+
+
+
