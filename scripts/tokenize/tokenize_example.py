fd3b1477437626a8115594f3acc6a4df8c72717c
bambooforest <bambooforest@gmail.com>
2013-11-03 23:22:01 +0100

    updated for multi-paths

--- scripts/tokenize/tokenize_example.py
+++ scripts/tokenize/tokenize_example.py
@@ -10,26 +10,30 @@ from lingpy import *
 
 word = "Màttís List"
 
-# t = Tokenizer("../../data/orthography_profiles/test.prf")
-# t = Tokenizer("test.prf")
-t = Tokenizer("test2")
+paths = ["../../data/orthography_profiles/test.prf", "test.prf", "test2"]
 
+for path in paths:
+    t = Tokenizer(path)
+    print()
+    print("test: t=Tokenizer("+path+")")
+    print()
+    print(word+"\t\t\t"+"word")
+    print(t.characters(word)+"\t\t"+"Tokenizer.characters(word)")
+    print(t.grapheme_clusters(word)+"\t\t"+"Tokenizer.grapheme_clusters(word)")
+    print(t.graphemes(word)+"\t\t"+"Tokenizer.graphemes(word)")
+    print(t.transform(word)+"\t\t"+"Tokenizer.transform(word) # defaults to graphemes")
+    print(t.transform(word, "ipa")+"\t\t"+"Tokenizer.transform(word, 'ipa')")
+    print(t.transform(word, "funny")+"\t\t"+"Tokenizer.transform(word, 'funny')")
+    print(t.rules(word)+"\t\t\t\t"+"Tokenizer.rules(word)")
+    print(t.transform_rules(word)+"\t\t\t"+"Tokenizer.transform_rules(word)")
+    print("====================")
+
+# empty tokenizer case
+print()
+t = Tokenizer()
+print("test: t=Tokenizer() # functions that require OPs raise exceptions")
 print()
 print(word+"\t\t\t"+"word")
 print(t.characters(word)+"\t\t"+"Tokenizer.characters(word)")
 print(t.grapheme_clusters(word)+"\t\t"+"Tokenizer.grapheme_clusters(word)")
-print(t.graphemes(word)+"\t\t"+"Tokenizer.graphemes(word)")
-print(t.transform(word)+"\t\t"+"Tokenizer.transform(word) # defaults to graphemes")
-print(t.transform(word, "ipa")+"\t\t"+"Tokenizer.transform(word, 'ipa')")
-print(t.transform(word, "funny")+"\t\t"+"Tokenizer.transform(word, 'funny')")
-print(t.rules(word)+"\t\t\t\t"+"Tokenizer.rules(word)")
-print(t.transform_rules(word)+"\t\t\t"+"Tokenizer.transform_rules(word)")
 
-"""
-print()
-t2 = Tokenizer()
-print()
-print(word+"\t\t\t"+"word")
-print(t2.characters(word)+"\t\t"+"Tokenizer.characters(word)")
-print(t2.grapheme_clusters(word)+"\t\t"+"Tokenizer.grapheme_clusters(word)")
-"""
