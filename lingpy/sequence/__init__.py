70480a063b8bd6145df2164dc1ea05270126185c
LinguList <mattis.list@posteo.de>
2013-11-04 22:39:41 +0100

    changed tokenize function

--- lingpy/sequence/__init__.py
+++ lingpy/sequence/__init__.py
@@ -1,13 +1,13 @@
 # author   : Johann-Mattis List
 # email    : mattis.list@gmail.com
 # created  : 2013-03-04 14:04
-# modified : 2013-03-04 14:04
+# modified : 2013-11-04 22:39
 """
 Module provides methods and functions for dealing with linguistic sequences.
 """
 
 __author__="Johann-Mattis List"
-__date__="2013-03-04"
+__date__="2013-11-04"
 
 # import for basic import
 from ..settings import rcParams
@@ -25,8 +25,37 @@ def tokenize(
         model=False,
         **keywords
         ):
+    """
+    Parameters
+    ----------
+    sequence : str
+        The input sequence that shall be tokenized.
+    orthography : {"asjp","fyzzy_ipa","plain_ipa"}(default="fuzzy_ipa")
+        The name of the orthography that is assumed for the input string.
+    model : {"sca","dolgo","asjp"}
+        The name of the sound-class model according to whicht the string shall
+        be converted.
+    merge_vowels : bool (default=True)
+        Specify, whether vowels shall be merged (only works with "asjp" and
+        "ipa" as orthography).
+    tokenizer : "str"
+        Name of the orthography profile that shall be used for tokenization
+        (does currently not support the conversion into sound class models).
+
+    Examples
+    --------
+    
+    >>> from lingpy import *
+    >>> seq = "p͡fyt͡sə"
+    >>> tokenize(seq)
+    ['p͡f', 'y', 't͡s', 'ə']
+    >>> tokenize(seq,model='sca')
+    ['B', 'Y', 'C', 'E']
+
+    """
     defaults = dict(
-            merge_vowels = True
+            merge_vowels = True,
+            tokenizer    = False
             )
     for k in defaults:
         if k not in keywords:
@@ -39,27 +68,26 @@ def tokenize(
             'fuzzy',
             'plain',
             'asjp'
-            ]:
+            ] and not keywords['tokenizer']:
         raise ValueError('[!] Orthography {0} not available.'.format(
                     orthography)
                     )
-
-    # return split of already tokenized stuff if string is already tokenized
-    if ' ' in sequence:
-        tokens = sequence.split(' ')
-    elif orthography in ['fuzzy','fuzzy_ipa']:
-        tokens = ipa2tokens(sequence)
-    elif orthography in ['plain','plain_ipa']:
-        tokens = _ipa_tokenizer.graphemes(sequence)
-    elif orthography in ['asjp']:
-        tokens = _asjp_tokenizer(sequence)
     
-    # check for existing model
+    if keywords['tokenizer']:
+        tfunc = lambda x: keywords['tokenizer'].graphemes(x)
+    elif orthography in ['fuzzy','fuzzy_ipa']:
+        tfunc = lambda x: ipa2tokens(x,**keywords)
+    elif orthography in ['plain','plain_ipa']:
+        tfunc = lambda x: _ipa_tokenizer.tokenize_ipa(x).split(' ')
+    elif orthography in ['asjp','asjp_code']:
+        tfunc = lambda x: _asjp_tokenizer.graphemes(x).split(' ')
+
     if not model:
-        return tokens
-    elif model in rcParams:
-        return tokens2class(tokens,rcParams[model])
+        return tfunc(sequence)
     else:
-        return tokens2class(tokens,model)
+        if model in rcParams:
+            return tokens2class(tfunc(sequence),rcParams[model])
+        else:
+            return tokens2class(tfunc(sequence),model)
 
 
