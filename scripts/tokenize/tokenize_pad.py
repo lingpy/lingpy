8e9f0d446049dcfb1611eaa866e828b02fe94b6d
bambooforest <bambooforest@gmail.com>
2013-11-05 10:00:39 +0100

    added tokenize pad example

---
+++ scripts/tokenize/tokenize_pad.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Test script for the orthography / tokenization module.
+"""
+
+__author__ = "Steven Moran"
+__date__ = "2010-12-01"
+
+from lingpy import *
+
+wl = Wordlist("pad_data_qlc.qlc")
+
+# wl.tokenize('pad_orthography_profile')
+
