9f615cd851fcfc4e0d26ff855146f9268377ad46
jdellert <johannes.dellert@student.uni-tuebingen.de>
2014-06-30 17:36:33 +0200

    Samoyedic test data now in readable format, test script executing the LingPy toolchain on them.

--- test/evolaemp/corr/sound_correspondence_test.py
+++ test/evolaemp/corr/sound_correspondence_test.py
@@ -1,2 +1,8 @@
 from numpy import *
-from lingpy2 import *
+from lingpy import *
+
+lex = LexStat('../data/eurasian/samoyedic.csv')
+
+print("Loaded " + str(len(lex.concept)) + " concepts in " + str(len(lex.language)) + " languages: " + ", ".join(lex.language));
+
+lex.get_scorer()
---
+++ test/evolaemp/data/eurasian/toolchain_test_samoyedic.py
@@ -0,0 +1,24 @@
+from numpy import *
+from lingpy import *
+
+lex = LexStat('samoyedic.csv')
+
+print("Loaded " + str(len(lex.concept)) + " concepts in " + str(len(lex.language)) + " languages: " + ", ".join(lex.language));
+
+lex.get_scorer()
+
+lex.cluster(method='lexstat', threshold=0.6)
+
+lex.calculate('tree', ref='lexstatid', tree_calc='neighbor')
+
+print(lex.tree.asciiArt())
+
+lex.output('qlc', filename='samoyedic-results', subset=True, formatter='concept', cols=['concept', 'taxa', 'counterpart', 'tokens', 'lexstatid'], ignore=["scorer"])
+
+alm = Alignments('samoyedic-results.qlc', ref='lexstatid')
+
+alm.align(method='library', output=False)
+
+alm.output("html", filename='samoyedic-alignments', ref='lexstatid')
+
+print("Alignment output as HTML complete!")
