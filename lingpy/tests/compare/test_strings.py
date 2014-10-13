320446981c227ae13fd877a35641bdfa6fe3a31b
LinguList <mattis.list@posteo.de>
2014-10-13 14:06:08 +0200

    commit first test for strings module

---
+++ lingpy/tests/compare/test_strings.py
@@ -0,0 +1,93 @@
+# author   : Taraka Rama, Johann-Mattis List
+# email    : mattis.list@uni-marburg.de
+# created  : 2014-10-13 13:49
+# modified : 2014-10-13 13:49
+"""
+Test routines for string comparisons.
+"""
+
+__author__="Taraka Rama, Johann-Mattis List"
+__date__="2014-10-13"
+
+
+from lingpy.compare import strings
+
+class TestStrings:
+    
+    def setup(self):
+
+        self.a = 'levenshtein'
+        self.b = 'lveenshtiene'
+    
+    def test_ldn(self):
+        d = strings.ldn(self.a,self.b)
+        assert d == 5
+        
+    def test_ldn_swap(self):
+        d = strings.ldn_swap(self.a,self.b)
+        assert d == 3
+
+    def test_bidist1(self):
+        strings.bidist1(self.a,self.b)
+
+    def test_tridist1(self):
+        strings.tridist1(self.a,self.b)
+
+    def test_bidist2(self):
+        strings.bidist2(self.a,self.b)
+
+    def test_tridist2(self):
+        strings.tridist2(self.a,self.b)
+
+    def test_bidist3(self):
+        strings.bidist3(self.a,self.b)
+
+    def test_tridist3(self):
+        strings.tridist3(self.a,self.b)
+
+    def test_dice(self):
+        strings.dice(self.a,self.b)
+
+    def test_lcs(self):
+        strings.lcs(self.a,self.b)
+
+    def test_bisim1(self):
+        strings.bisim1(self.a,self.b)
+
+    def test_trisim1(self):
+        strings.trisim1(self.a,self.b)
+
+    def test_bisim2(self):
+        strings.bisim2(self.a,self.b)
+
+    def test_trisim2(self):
+        strings.trisim2(self.a,self.b)
+
+    def test_bisim3(self):
+        strings.bisim3(self.a,self.b)
+
+    def test_trisim3(self):
+        strings.trisim3(self.a,self.b)
+
+    def test_jcd(self):
+        strings.jcd(self.a,self.b)
+
+    def test_jcdn(self):
+        strings.jcdn(self.a,self.b)
+
+    def test_prefix(self):
+        strings.prefix(self.a,self.b)
+
+    def test_xdice(self):
+        strings.xdice(self.a,self.b)
+
+    def test_trigram(self):
+        strings.trigram(self.a,self.b)
+
+    def test_ident(self):
+        strings.ident(self.a,self.b)
+
+    def test_xxdice(self):
+        strings.xxdice(self.a,self.b)
+
+
