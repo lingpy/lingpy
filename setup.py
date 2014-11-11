ed9ad75cc87801bd4dab654f198f907838d43153
LinguList <mattis.list@posteo.de>
2014-11-11 12:20:03 +0100

    yet another attemtpt to remove old version

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ if sys.version_info < (3, 4):
     requires.append('pathlib')
 
 # make global name of this version for convenience of modifying it
-thisversion = "2.4.1.dev"
+thisversion = "2.4.2.dev"
 
 setup(
     name=pkgname,
