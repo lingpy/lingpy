d36f7260bc4f3287d4e1802e05b2aaa7cc47d6d7
xrotwang <xrotwang@googlemail.com>
2014-11-11 12:10:48 +0100

    more fiddling

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -29,7 +29,7 @@ MOCK_MODULES = [
     'scipy.cluster.hierarchy',
     'scipy.stats']
 for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
+    sys.modules[mod_name] = mock.MagicMock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
