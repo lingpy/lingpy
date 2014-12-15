e577f04bbe2d1b8f4240ac864303b563af635f4d
LinguList <mattis.list@posteo.de>
2014-12-15 21:18:11 +0100

    update tests of read/csv.py to bring coverage to 100%

--- lingpy/tests/read/test_csv.py
+++ lingpy/tests/read/test_csv.py
@@ -12,6 +12,7 @@ __date__="2013-11-23"
 import os
 
 from six import text_type
+from nose.tools import assert_raises
 
 from lingpy.read.csv import *
 from lingpy.compare.lexstat import LexStat
@@ -22,6 +23,14 @@ from ..util import *
 def test_read_asjp():
     lex = LexStat(read_asjp(
         test_data('asjp_test_list.csv'), family="CELTIC", classification="wls_gen"))
+    assert len(lex) == 249
+    
+    evaluate = lambda x,y,z: x[y[1]].startswith(z)
+
+    lex = LexStat(read_asjp(
+        test_data('asjp_test_list.csv'), family='CELTIC',
+        classification='wls_fam,wls_gen', evaluate=evaluate))
+    
     assert len(lex) == 249
 
 
@@ -61,6 +70,8 @@ def test_csv2list():
     assert dat3[0][0] == 'This\tis\tthe'
     assert dat4[3][0] == '#I'
 
+    assert_raises(NameError, csv2list, 'xyz.tsv')
+
 def test_csv2dict():
 
     if_path1 = os.path.join(test_data(), 'test_csv.csv')
@@ -86,6 +97,43 @@ def test_csv2dict():
 
     # modify the comment char
     dat4 = csv2dict(
+            if_path1,
+            comment = "?"
+            )
+
+    # check for correct results
+    assert 'This' in dat1
+    assert 'This' not in dat2
+    assert dat2['We'][2] == 2
+    assert dat3['This\tis\tthe'][0] == 'head\tline'
+    assert len(dat4) == 4 and '#I' in dat4
+
+# test the dummy alias which should probably be killed from the code
+def test_read_csv():
+
+    if_path1 = os.path.join(test_data(), 'test_csv.csv')
+    if_path2 = os.path.join(test_data(), 'test_csv')
+
+    # check default setting
+    dat1 = csv2dict(if_path1)
+
+    # pass data type and header
+    dat2 = read_csv(
+            if_path2,
+            fileformat = 'csv',
+            dtype = [text_type, text_type, text_type, int, text_type],
+            sep = '\t',
+            header = True
+            )
+
+    # pass another separator
+    dat3 = read_csv(
+            if_path1,
+            sep = '_'
+            )
+
+    # modify the comment char
+    dat4 = read_csv(
             if_path1,
             comment = "?"
             )
