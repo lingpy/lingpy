84ce1b17cea598f9e33c4d10fab4bdb808589782
LinguList <mattis.list@uni-marburg.de>
2014-12-23 17:24:34 +0100

    added brief docu to read_text_file

--- lingpy/util.py
+++ lingpy/util.py
@@ -86,10 +86,13 @@ class TextFile(object):
 
 def read_text_file(path, normalize=None, lines=False):
     """Read a text file encoded in utf-8.
-
+    
+    Parameters
+    ----------
     :param path: File-system path of the file.
     :param normalize: If not `None` a valid unicode normalization mode must be passed.
-    :param lines: Flag signalling whether to return a list of lines.
+    :param lines: Flag signalling whether to return a list of lines (without 
+      the line-separation character).
     :return: File content as unicode object or list of lines as unicode objects.
 
     .. note:: The whole file is read into memory.
