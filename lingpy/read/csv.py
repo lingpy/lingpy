10c53bbb0b1e4feef9ee7448e511abb0066fb126
xrotwang <xrotwang@googlemail.com>
2016-03-16 11:40:59 +0100

    pep8 lingpy/read and lingpy/sequence

--- lingpy/read/csv.py
+++ lingpy/read/csv.py
@@ -1,23 +1,23 @@
 """
 Module provides functions for reading csv-files.
 """
+from __future__ import unicode_literals, division, print_function
 
-from ..util import read_text_file
-from ..settings import rcParams
-from ..sequence.sound_classes import asjp2tokens
-import re
+from lingpy.util import read_text_file
+from lingpy.sequence.sound_classes import asjp2tokens
+
 
 def csv2list(
-        filename,
-        fileformat = '',
-        dtype = None,
-        comment = '#',
-        sep = '\t',
-        strip_lines = True,
-        header = False
-        ):
+    filename,
+    fileformat='',
+    dtype=None,
+    comment='#',
+    sep='\t',
+    strip_lines=True,
+    header=False
+):
     """
-    Very simple function to get quick access to CSV-files.
+    Very simple function to get quick (and somewhat naive) access to CSV-files.
 
     Parameters
     ----------
@@ -51,24 +51,22 @@ def csv2list(
     """
     # check for correct fileformat
     if fileformat:
-        infile = filename+'.'+fileformat
+        infile = filename + '.' + fileformat
     else:
         infile = filename
 
-    if dtype is None: dtype = []
+    if dtype is None:
+        dtype = []
 
     l = []
-    
-    # open the file
-    infile = read_text_file(infile, lines=True, normalize="NFC") 
-    
-    # check for header
-    if header:
-        idx = 0
-    else:
-        idx = -1
 
-    for i,line in enumerate(infile):
+    # open the file
+    infile = read_text_file(infile, lines=True, normalize="NFC")
+
+    # check for header
+    idx = 0 if header else -1
+
+    for i, line in enumerate(infile):
         if line and not line.startswith(comment) and idx != i:
             if strip_lines:
                 cells = [c.strip() for c in line.strip().split(sep)]
@@ -77,19 +75,20 @@ def csv2list(
             if not dtype:
                 l += [cells]
             else:
-                l += [[f(c) for f,c in zip(dtype,cells)]]
+                l += [[f(c) for f, c in zip(dtype, cells)]]
 
     return l
 
+
 def csv2dict(
-        filename,
-        fileformat = None,
-        dtype = None,
-        comment = '#',
-        sep = '\t',
-        strip_lines = True,
-        header = False
-        ):
+    filename,
+    fileformat=None,
+    dtype=None,
+    comment='#',
+    sep='\t',
+    strip_lines=True,
+    header=False
+):
     """
     Very simple function to get quick access to CSV-files.
 
@@ -123,113 +122,99 @@ def csv2dict(
         A dictionary-representation of the CSV file, with the first row being
         used as key and the rest of the rows as values.
     """
-    
+
     l = csv2list(filename, fileformat, dtype, comment, sep, strip_lines, header)
+    return {line[0]: line[1:] for line in l}
 
-    d = {}
 
-    for line in l:
-        d[line[0]] = line[1:]
-
-    return d               
-
-def csv2multidict(
-        filename,
-        comment = '#',
-        sep = '\t'
-        ):
+def csv2multidict(filename, comment='#', sep='\t'):
     """
     Function reads a csv-file into a multi-dimensional dictionary structure.
     """
-    mdict = {}
     tsv = csv2list(filename, comment=comment, sep=sep)
     header = tsv[0]
-    for line in tsv[1:]:
-        mdict[line[0]] = dict(zip(header[1:], line[1:]))
+    return {line[0]: dict(zip(header[1:], line[1:])) for line in tsv[1:]}
 
-    return mdict
 
 def read_asjp(
-        infile,
-        family = 'Indo-European',
-        classification = 'hh',
-        max_synonyms = 2,
-        min_population = lambda x: x > 0 or abs(x) > 1900,
-        merge_vowels=True,
-        evaluate = False
-        ):
-    
+    infile,
+    family='Indo-European',
+    classification='hh',
+    max_synonyms=2,
+    min_population=lambda x: x > 0 or abs(x) > 1900,
+    merge_vowels=True,
+    evaluate=False
+):
     # read in all data
-    data = csv2list(infile,strip_lines=False)
-    
+    data = csv2list(infile, strip_lines=False)
+
     # find the data confirming to selection, get the header for the
     # classification first
     header = [h.lower() for h in data[0]]
-    
+
     # check for family type
     if not evaluate:
-        evaluate = lambda x,y,z:x[y].startswith(z)
+        evaluate = lambda x, y, z: x[y].startswith(z)
 
     # index the classification index
     if ',' in classification:
-        a,b = classification.split(',')
+        a, b = classification.split(',')
         clsA_idx = header.index(a)
         clsB_idx = header.index(b)
-        cls_idx = (clsA_idx,clsB_idx)
+        cls_idx = (clsA_idx, clsB_idx)
     else:
         cls_idx = header.index(classification)
-    
+
     # create dictionary to store the data
-    D,idx = {},1
+    D, idx = {}, 1
 
     # lats / longs
     meta = dict(
-            coords = {},
-            iso = {},
-            population = {},
-            classification = dict(
-                hammarstroem = {},
-                ethnologue = {},
-                wals = {},
-                wals_genus = {}
-                )
-            )
+        coords={},
+        iso={},
+        population={},
+        classification=dict(
+            hammarstroem={},
+            ethnologue={},
+            wals={},
+            wals_genus={}
+        )
+    )
 
     # iterate over data and extract the lines
     for line in data[1:]:
-        if evaluate(line,cls_idx,family):
+        if evaluate(line, cls_idx, family):
             lang = line[0].strip()
             wls = line[1].strip()
             wls_gen = line[2].strip()
             eth = line[3].strip()
             hh = line[4].strip()
             try:
-                lat = float(line[5].replace(',','.'))
-                lng = float(line[6].replace(',','.'))
+                lat = float(line[5].replace(',', '.'))
+                lng = float(line[6].replace(',', '.'))
             except:
                 lat = ''
                 lng = ''
             pop = int(line[7]) if line[7] else -10
             iso = line[9].strip()
-            
+
             # check for population
-            if min_population(pop): # >= min_population:
+            if min_population(pop):  # >= min_population:
                 # append data to meta
-                if not lang in meta['coords']:
-                    meta["coords"][lang] = (lat,lng)
+                if lang not in meta['coords']:
+                    meta["coords"][lang] = (lat, lng)
                     meta["classification"]["hammarstroem"][lang] = hh
-                    meta["classification"]["wals"][lang] =  wls
+                    meta["classification"]["wals"][lang] = wls
                     meta["classification"]["wals_genus"][lang] = wls_gen
                     meta["classification"]["ethnologue"][lang] = eth
                     meta["population"][lang] = pop
                     meta["iso"][lang] = iso
 
-                    for i,items in enumerate(line[10:],10):
-
+                    for i, items in enumerate(line[10:], 10):
                         item = header[i].strip()
                         entries = [e.strip() for e in line[i].split(',') if
-                                e.strip() and 'xxx' not in e.lower()][:max_synonyms]
-                    
+                                   e.strip() and 'xxx' not in e.lower()][:max_synonyms]
+
                         for entry in entries:
                             if entry.startswith('%'):
                                 entry = entry[1:]
@@ -237,36 +222,10 @@ def read_asjp(
                             else:
                                 loan = 0
                             if ' ' in entry:
-                                entry = entry.replace(' ','_')
+                                entry = entry.replace(' ', '_')
                             tokens = ' '.join(asjp2tokens(entry))
-                            #tokens = ' '.join(
-                            #            ipa2tokens(
-                            #                entry,
-                            #                diacritics = '*$~"',
-                            #                vowels = 'aeiouE3',
-                            #                tones = '',
-                            #                combiners = '',
-                            #                merge_vowels = merge_vowels#rcParams['merge_vowels']
-                            #                )
-                            #            )
-                            #tokens = re.sub(r'([^ ]) ([^ ])~',r'\1\2~',tokens)
-                            #tokens = re.sub(r'([^ ]) ([^ ]) ([^ ])\$',r'\1\2\3$',tokens)
-
-                            D[idx] = [
-                                    lang,
-                                    item,
-                                    entry,
-                                    tokens.split(' '),
-                                    loan
-                                    ]
+                            D[idx] = [lang, item, entry, tokens.split(' '), loan]
                             idx += 1
-    D[0] = ['doculect','concept','counterpart','tokens','known_borrowings']
+    D[0] = ['doculect', 'concept', 'counterpart', 'tokens', 'known_borrowings']
     D['meta'] = meta
-
     return D
-                                
-
-                    
-
-
-
