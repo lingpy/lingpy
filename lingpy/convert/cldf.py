707c2a7dadbe693c780088e8961c596dff90af4a
lingulist <mattis.list@lingpy.org>
2018-03-07 16:49:41 +0100

    update cldf

--- lingpy/convert/cldf.py
+++ lingpy/convert/cldf.py
@@ -11,7 +11,7 @@ from csvw.dsv import reader
 from clldutils.misc import slug
 
 from lingpy import util
-from lingpy.basic.wordlist import Wordlist
+from lingpy.basic.wordlist import Wordlist, from_cldf
 from lingpy.convert.html import template_path
 
 try:
@@ -21,68 +21,6 @@ except ImportError:
     cldf = False
 
 
-def from_cldf(path, to=Wordlist):
-    """
-    Load data from CLDF into a LingPy Wordlist object or similar.
-
-    Parameters
-    ----------
-    path : str
-        The path to the metadata-file of your CLDF dataset.
-    to : ~lingpy.basic.wordlist.Wordlist
-        A ~lingpy.basic.wordlist.Wordlist object or one of the descendants
-        (LexStat, Alignmnent).
-
-    Note
-    ----
-    This function does not offer absolute flexibility regarding the data you
-    can input so far. However, it can regularly read CLDF-formatted data into
-    LingPy and thus allow you to use CLDF data in LingPy analyses.
-
-    Todo
-    ----
-    Add support for partial cognates.
-    """
-    tbg = TableGroup.from_file(path)
-    forms = tbg.tabledict['forms.csv']
-
-    # obtain the dictionaries to convert ids to values
-    taxa = {t['ID']: (t['Name'], t['glottocode']) for t in
-            tbg.tabledict['languages.csv']}
-    concepts = {c['ID']: (c['Name'], c['Concepticon_ID']) for c in 
-            tbg.tabledict['parameters.csv']}
-
-    # create dictionary
-    D = {}
-    id2idx = {}
-    for i, row in enumerate(forms):
-        # check for numeric ID
-        if row['ID'].isdigit:
-            idx = int(row['ID'])
-        else:
-            idx = i+1
-        id2idx[row['ID']] = idx
-
-        doculect, glottocode = taxa[row['Language_ID']]
-        concept, concepticon_id = concepts[row['Parameter_ID']]
-        D[idx] = [doculect, glottocode, concept, concepticon_id] + [row.get(f,
-            '') or '' for f in ['form_in_source', 'Form', 'Segments',
-                'Comment', 'Source']]
-    # add the header
-    D[0] = ['doculect', 'glottocode', 'concept', 'concepticon_id', 'value',
-            'form', 'tokens', 'note', 'source']
-
-    # convert to wordlist (simplifies handling)
-    wordlist = to(D)
-    
-    # add cognates if they are needed
-    if 'cognates.csv' in tbg.tabledict:
-        cognates = {id2idx[row['Form_ID']]: (row['Cognateset_ID'],
-            row['Alignment']) for row in tbg.tabledict['cognates.csv']}
-        wordlist.add_entries('cogid', cognates, lambda x: x[0] or 0)
-        wordlist.add_entries('alignment', cognates, lambda x: x[1] or '')
-
-    return wordlist
 
 
 def to_cldf(wordlist, path='cldf', source_path=None, ref="cogid",
