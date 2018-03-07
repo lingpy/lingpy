ee1f78638245812c294f4e2a1f8ce7129ee35362
lingulist <mattis.list@lingpy.org>
2018-03-07 17:20:41 +0100

    fixing one test

--- lingpy/tests/convert/test_cldf.py
+++ lingpy/tests/convert/test_cldf.py
@@ -4,6 +4,7 @@ from lingpy.tests.util import test_data
 def test_from_cldf():
     from lingpy.basic.wordlist import from_cldf
     wl = from_cldf(test_data('cldf/test-metadata.json'), language='Name',
-            concept='Name', concepticon="Concepticon_ID")
+            concept='Name', concepticon="Concepticon_ID",
+            glottocode='glottocode')
     assert wl.width == 29
     assert wl.height == 1
