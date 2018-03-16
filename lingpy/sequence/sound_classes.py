b107f832517eb6ec418779b9db973c0f8f041144
Simon J Greenhill <SimonGreenhill@users.noreply.github.com>
2018-03-16 09:28:37 +0100

    Update sound_classes.py

--- lingpy/sequence/sound_classes.py
+++ lingpy/sequence/sound_classes.py
@@ -1157,7 +1157,7 @@ def class2tokens(tokens, classes, gap_char='-', local=False):
     """
     if not local:
         out = [t for t in tokens]
-        for i in enumerate(classes):
+        for i, c in enumerate(classes):
             if c in '-X':
                 out.insert(i, gap_char)
     else:
