94009528636fe7f67d43c5c0abd9fbe25ad01f03
Christoph Rzymski <christoph@functor-argument.net>
2018-04-09 11:25:56 +0200

    More robust imports for tests, minor test- clean-up (#351)

--- lingpy/tests/read/test_csv.py
+++ lingpy/tests/read/test_csv.py
@@ -1,74 +1,73 @@
 """
 Tests for the read.csv module.
 """
+__author__ = "Johann-Mattis List"
+__date__ = "2013-11-23"
+
 from six import text_type
-from nose.tools import assert_raises
-from lingpy.read.csv import *
 
-__author__="Johann-Mattis List"
-__date__="2013-11-23"
-
-from lingpy.read.csv import read_asjp
 from lingpy.compare.lexstat import LexStat
-from ..util import *
+from lingpy.read.csv import *
+from lingpy.tests.util import test_data
+
 
 def test_read_asjp():
     lex = LexStat(read_asjp(
-        test_data('asjp_test_list.csv'), family="CELTIC", classification="wls_gen"))
+        test_data('asjp_test_list.csv'), family="CELTIC",
+        classification="wls_gen"))
     assert len(lex) == 249
-    
-    evaluate = lambda x,y,z: x[y[1]].startswith(z)
+
+    def evaluate():
+        return lambda x, y, z: x[y[1]].startswith(z)
 
     lex = LexStat(read_asjp(
         test_data('asjp_test_list.csv'), family='GERMANIC',
-        classification='wls_fam,wls_gen', evaluate=evaluate))
-    
+        classification='wls_fam,wls_gen', evaluate=evaluate()))
+
     assert len(lex) == 1429
 
     # check if loans have been traced and if at least one word is represented
     # as expected
     entry = lex.get_dict(doculect="YIDDISH_EASTERN")
     idx = entry['person'][0]
-    assert lex[idx,'known_borrowings'] == 1
-    assert lex[idx,'counterpart'] == "pErzon"
-from six import text_type
-import os
-from lingpy.tests.util import test_data
+    assert lex[idx, 'known_borrowings'] == 1
+    assert lex[idx, 'counterpart'] == "pErzon"
 
 
 def test_read_asjp():
     lex = LexStat(read_asjp(
-        test_data('asjp_test_list.csv'), family="CELTIC", classification="wls_gen"))
+        test_data('asjp_test_list.csv'), family="CELTIC",
+        classification="wls_gen"))
     assert len(lex) == 249
 
-def test_csv2list():
 
+def test_csv2list():
     if_path1 = test_data('test_csv.csv')
     if_path2 = test_data('test_csv')
-    
+
     # check default setting
     dat1 = csv2list(if_path1)
 
     # pass data type and header
     dat2 = csv2list(
-            if_path2,
-            fileformat = 'csv',
-            dtype = [text_type, text_type, text_type, int, text_type],
-            sep = '\t',
-            header = True
-            )
+        if_path2,
+        fileformat='csv',
+        dtype=[text_type, text_type, text_type, int, text_type],
+        sep='\t',
+        header=True
+    )
 
     # pass another separator
     dat3 = csv2list(
-            if_path1,
-            sep = '_'
-            )
+        if_path1,
+        sep='_'
+    )
 
     # modify the comment char
     dat4 = csv2list(
-            if_path1,
-            comment = "?"
-            )
+        if_path1,
+        comment="?"
+    )
 
     # check for correct parsing
     assert dat1[0][1] == 'is'
@@ -77,8 +76,8 @@ def test_csv2list():
     assert dat3[0][0] == 'This\tis\tthe'
     assert dat4[3][0] == '#I'
 
-def test_csv2dict():
 
+def test_csv2dict():
     if_path1 = test_data('test_csv.csv')
     if_path2 = test_data('test_csv')
 
@@ -87,24 +86,24 @@ def test_csv2dict():
 
     # pass data type and header
     dat2 = csv2dict(
-            if_path2,
-            fileformat = 'csv',
-            dtype = [text_type, text_type, text_type, int, text_type],
-            sep = '\t',
-            header = True
-            )
+        if_path2,
+        fileformat='csv',
+        dtype=[text_type, text_type, text_type, int, text_type],
+        sep='\t',
+        header=True
+    )
 
     # pass another separator
     dat3 = csv2dict(
-            if_path1,
-            sep = '_'
-            )
+        if_path1,
+        sep='_'
+    )
 
     # modify the comment char
     dat4 = csv2dict(
-            if_path1,
-            comment = "?"
-            )
+        if_path1,
+        comment="?"
+    )
 
     # check for correct results
     assert 'This' in dat1
@@ -113,10 +112,10 @@ def test_csv2dict():
     assert dat3['This\tis\tthe'][0] == 'head\tline'
     assert len(dat4) == 4 and '#I' in dat4
 
-def test_csv2multidict():
 
+def test_csv2multidict():
     if_path1 = test_data('test_csv.csv')
-    
+
     md = csv2multidict(if_path1)
 
     assert md['We']['is'] == 'are'
--- lingpy/tests/sequence/test_profile.py
+++ lingpy/tests/sequence/test_profile.py
@@ -1,12 +1,10 @@
-# *-* coding: utf-8 *-* 
-from __future__ import print_function
 from __future__ import division
+from __future__ import print_function
 from __future__ import unicode_literals
-from ..util import test_data
-from nose.tools import assert_raises
-from six import text_type
+
 from lingpy.basic.wordlist import Wordlist
 from lingpy.sequence.profile import simple_profile, context_profile
+from lingpy.tests.util import test_data
 
 
 def test_simple_profile():
@@ -16,10 +14,10 @@ def test_simple_profile():
     prf = list(simple_profile(wl, clts={'a': 'A'}))
     assert prf[0][1] == 'A'
 
+
 def test_context_profile():
     wl = Wordlist(test_data('KSL6.qlc'))
     prf = list(context_profile(wl))
-    assert prf[2][-2] == '4' # first line of profile
+    assert prf[2][-2] == '4'  # first line of profile
     prf = list(context_profile(wl, clts={'a': 'A'}))
     assert prf[2][1] == 'A'
-
