a979ac40d55dd84ca76b32f463da67b69dff8518
Christoph Rzymski <christoph@functor-argument.net>
2018-04-12 10:10:02 +0200

    Refactorings for convert tests.

--- lingpy/tests/convert/test_cldf.py
+++ lingpy/tests/convert/test_cldf.py
@@ -1,10 +1,17 @@
-from lingpy.tests.util_testing import WithTempDir
+from unittest import TestCase
+
+from lingpy.basic.wordlist import from_cldf
 from lingpy.tests.util import test_data
 
-def test_from_cldf():
-    from lingpy.basic.wordlist import from_cldf
-    wl = from_cldf(test_data('cldf/test-metadata.json'), language='Name',
-            concept='Name', concepticon="Concepticon_ID",
-            glottocode='glottocode')
-    assert wl.width == 29
-    assert wl.height == 1
+
+class Tests(TestCase):
+    def test_from_cldf(self):
+        wl = from_cldf(test_data('cldf/test-metadata.json'), language='Name',
+                       concept='Name', concepticon="Concepticon_ID",
+                       glottocode='glottocode')
+
+        assert wl.width == 29
+        assert wl.height == 1
+        assert wl.entries[0] == 'alignment'
+        assert wl.cols[0] == 'Anuta'
+        assert wl.cols[28] == 'Vaeakau-Taumako'
--- lingpy/tests/convert/test_graph.py
+++ lingpy/tests/convert/test_graph.py
@@ -1,3 +1,5 @@
+from unittest import TestCase
+
 import lingpy.convert.graph as lcg
 
 try:
@@ -6,31 +8,32 @@ try:
     nonetworkx = False
 except ImportError:
     nonetworkx = True
+
 try:
-    import igraph as ig
+    import jgraph as ig
 
     noigraph = False
 except ImportError:
     noigraph = True
 
 
-def test_networkx2igraph():
-    if not noigraph and not nonetworkx:
-        G = nx.Graph()
-        G.add_nodes_from(['a', 'b', 'c'])
-        G.add_edges_from([('a', 'b'), ('c', 'd')])
+class Tests(TestCase):
+    def test_networkx2igraph(self):
+        if not noigraph and not nonetworkx:
+            graph = nx.Graph()
+            graph.add_nodes_from(['a', 'b', 'c'])
+            graph.add_edges_from([('a', 'b'), ('c', 'd')])
 
-        N = lcg.networkx2igraph(G)
-        assert len(N.vs) == 4
-        assert len(N.es) == 2
+            network = lcg.networkx2igraph(graph)
+            assert len(network.vs) == 4
+            assert len(network.es) == 2
 
+    def test_igraph2networkx(self):
+        if not noigraph and not nonetworkx:
+            graph = ig.Graph()
+            graph.add_vertices(['a', 'b', 'c', 'd'])
+            graph.add_edges([('a', 'b'), ('c', 'd')])
 
-def test_igraph2networkx():
-    if not noigraph and not nonetworkx:
-        G = ig.Graph()
-        G.add_vertices(['a', 'b', 'c', 'd'])
-        G.add_edges([('a', 'b'), ('c', 'd')])
-
-        N = lcg.igraph2networkx(G)
-        assert len(list(N.nodes())) == 4
-        assert len(list(N.edges())) == 2
+            network = lcg.igraph2networkx(graph)
+            assert len(list(network.nodes())) == 4
+            assert len(list(network.edges())) == 2
