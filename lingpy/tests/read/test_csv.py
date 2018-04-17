7149dc1f763e8ea2fb11b904f0ec9bde62bc2a68
chrzyki <christoph@functor-argument.net>
2018-04-17 11:25:09 +0200

    Refactorings for read test.

--- lingpy/tests/read/test_csv.py
+++ lingpy/tests/read/test_csv.py
@@ -7,113 +7,110 @@ from lingpy.compare.lexstat import LexStat
 from lingpy.read.csv import *
 from lingpy.tests.util import test_data
 
-
-def test_read_asjp():
-    lex = LexStat(read_asjp(
-        test_data('asjp_test_list.csv'), family="CELTIC",
-        classification="wls_gen"))
-    assert len(lex) == 249
-
-    def evaluate():
-        return lambda x, y, z: x[y[1]].startswith(z)
-
-    lex = LexStat(read_asjp(
-        test_data('asjp_test_list.csv'), family='GERMANIC',
-        classification='wls_fam,wls_gen', evaluate=evaluate()))
-
-    assert len(lex) == 1429
-
-    # check if loans have been traced and if at least one word is represented
-    # as expected
-    entry = lex.get_dict(doculect="YIDDISH_EASTERN")
-    idx = entry['person'][0]
-    assert lex[idx, 'known_borrowings'] == 1
-    assert lex[idx, 'counterpart'] == "pErzon"
+from unittest import TestCase
 
 
-def test_read_asjp():
-    lex = LexStat(read_asjp(
-        test_data('asjp_test_list.csv'), family="CELTIC",
-        classification="wls_gen"))
-    assert len(lex) == 249
+class Tests(TestCase):
+    def setUp(self):
+        pass
 
+    def test_read_asjp(self):
+        lex = LexStat(read_asjp(
+            test_data('asjp_test_list.csv'), family='CELTIC',
+            classification='wls_gen'))
+        assert len(lex) == 249
 
-def test_csv2list():
-    if_path1 = test_data('test_csv.csv')
-    if_path2 = test_data('test_csv')
+        # def evaluate():
+        #    return lambda x, y, z: x[y[1]].startswith(z)
 
-    # check default setting
-    dat1 = csv2list(if_path1)
+        # TODO: Fix value entry, ask Mattis.
+        # lex = LexStat(read_asjp(
+        #    test_data('asjp_test_list.csv'), family='GERMANIC',
+        #    classification='wls_fam,wls_gen', evaluate=evaluate()))
 
-    # pass data type and header
-    dat2 = csv2list(
-        if_path2,
-        fileformat='csv',
-        dtype=[text_type, text_type, text_type, int, text_type],
-        sep='\t',
-        header=True
-    )
+        # assert len(lex) == 1429
 
-    # pass another separator
-    dat3 = csv2list(
-        if_path1,
-        sep='_'
-    )
+        # check if loans have been traced and if at least one word is
+        # represented as expected
+        # entry = lex.get_dict(doculect='YIDDISH_EASTERN')
+        # idx = entry['person'][0]
+        # assert lex[idx, 'known_borrowings'] == 1
+        # assert lex[idx, 'counterpart'] == "pErzon"
 
-    # modify the comment char
-    dat4 = csv2list(
-        if_path1,
-        comment="?"
-    )
+    def test_csv2list(self):
+        if_path1 = test_data('test_csv.csv')
+        if_path2 = test_data('test_csv')
 
-    # check for correct parsing
-    assert dat1[0][1] == 'is'
-    assert dat2[0][1] == 'are'
-    assert sum([x[3] for x in dat2]) == 8
-    assert dat3[0][0] == 'This\tis\tthe'
-    assert dat4[3][0] == '#I'
+        # check default setting
+        dat1 = csv2list(if_path1)
 
+        # pass data type and header
+        dat2 = csv2list(
+            if_path2,
+            fileformat='csv',
+            dtype=[text_type, text_type, text_type, int, text_type],
+            sep='\t',
+            header=True
+        )
 
-def test_csv2dict():
-    if_path1 = test_data('test_csv.csv')
-    if_path2 = test_data('test_csv')
+        # pass another separator
+        dat3 = csv2list(
+            if_path1,
+            sep='_'
+        )
 
-    # check default setting
-    dat1 = csv2dict(if_path1)
+        # modify the comment char
+        dat4 = csv2list(
+            if_path1,
+            comment="?"
+        )
 
-    # pass data type and header
-    dat2 = csv2dict(
-        if_path2,
-        fileformat='csv',
-        dtype=[text_type, text_type, text_type, int, text_type],
-        sep='\t',
-        header=True
-    )
+        # check for correct parsing
+        assert dat1[0][1] == 'is'
+        assert dat2[0][1] == 'are'
+        assert sum([x[3] for x in dat2]) == 8
+        assert dat3[0][0] == 'This\tis\tthe'
+        assert dat4[3][0] == '#I'
 
-    # pass another separator
-    dat3 = csv2dict(
-        if_path1,
-        sep='_'
-    )
+    def test_csv2dict(self):
+        if_path1 = test_data('test_csv.csv')
+        if_path2 = test_data('test_csv')
 
-    # modify the comment char
-    dat4 = csv2dict(
-        if_path1,
-        comment="?"
-    )
+        # check default setting
+        dat1 = csv2dict(if_path1)
 
-    # check for correct results
-    assert 'This' in dat1
-    assert 'This' not in dat2
-    assert dat2['We'][2] == 2
-    assert dat3['This\tis\tthe'][0] == 'head\tline'
-    assert len(dat4) == 4 and '#I' in dat4
+        # pass data type and header
+        dat2 = csv2dict(
+            if_path2,
+            fileformat='csv',
+            dtype=[text_type, text_type, text_type, int, text_type],
+            sep='\t',
+            header=True
+        )
 
+        # pass another separator
+        dat3 = csv2dict(
+            if_path1,
+            sep='_'
+        )
 
-def test_csv2multidict():
-    if_path1 = test_data('test_csv.csv')
+        # modify the comment char
+        dat4 = csv2dict(
+            if_path1,
+            comment="?"
+        )
 
-    md = csv2multidict(if_path1)
+        # check for correct results
+        assert 'This' in dat1
+        assert 'This' not in dat2
+        assert dat2['We'][2] == 2
+        assert dat3['This\tis\tthe'][0] == 'head\tline'
+        assert len(dat4) == 4 and '#I' in dat4
 
-    assert md['We']['is'] == 'are'
-    assert sum([int(md[x]['head']) for x in md]) == 8
+    def test_csv2multidict(self):
+        if_path1 = test_data('test_csv.csv')
+
+        md = csv2multidict(if_path1)
+
+        assert md['We']['is'] == 'are'
+        assert sum([int(md[x]['head']) for x in md]) == 8
