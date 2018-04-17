3987661e3264f689f42ad679d0b814b87da4d895
Christoph Rzymski <christoph@functor-argument.net>
2018-04-17 11:32:21 +0200

    Refactorings for read test. (#360)

--- lingpy/tests/read/test_qlc.py
+++ lingpy/tests/read/test_qlc.py
@@ -1,28 +1,31 @@
-from lingpy.read.qlc import read_msa, reduce_alignment, read_qlc
+from unittest import TestCase
+
 from lingpy.align import MSA
+from lingpy.read.qlc import read_msa, reduce_alignment, read_qlc
 from lingpy.tests.util import test_data
 
 
-def test_read_msa():
-    msa = MSA(read_msa(test_data('harry.msa')))
-    assert hasattr(msa, 'seqs')
+class Tests(TestCase):
+    def setUp(self):
+        pass
 
-def test_normalize_alignment():
+    def test_read_msa(self):
+        msa = MSA(read_msa(test_data('harry.msa')))
+        assert hasattr(msa, 'seqs')
 
-    msa = MSA(read_msa(test_data('harry_unnormal.msa')))
+    def test_normalize_alignment(self):
+        msa = MSA(read_msa(test_data('harry_unnormal.msa')))
 
-    for line in msa.alignment[1:]:
-        assert len(line) == len(msa.alignment[0])
+        for line in msa.alignment[1:]:
+            assert len(line) == len(msa.alignment[0])
 
-def test_reduce_msa():
+    def test_reduce_msa(self):
+        msa = MSA(read_msa(test_data('test_reduce.msa')))
+        reduced_alignment = reduce_alignment(msa.alignment)
+        for i, line in enumerate(reduced_alignment):
+            assert len(line) == 4 and \
+                    ''.join(line) == ''.join(
+                            msa.alignment[i])[:msa.alignment[i].index('(')]
 
-    msa = MSA(read_msa(test_data('test_reduce.msa')))
-    reduced_alignment = reduce_alignment(msa.alignment)
-    for i,line in enumerate(reduced_alignment):
-        assert len(line) == 4 and \
-                ''.join(line) == ''.join(
-                        msa.alignment[i])[:msa.alignment[i].index('(')]
-
-def test_read_qlc():
-
-    qlc = read_qlc(test_data('read_qlc.qlc'))
+    def test_read_qlc(self):
+        _ = read_qlc(test_data('read_qlc.qlc'))
