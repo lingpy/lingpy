d198bd10efe38863a0fa2c2891124b22db18af5e
chrzyki <christoph@functor-argument.net>
2018-04-19 11:04:30 +0200

    Minor clean-ups and refactorings in the general tests.

--- lingpy/tests/test_cache.py
+++ lingpy/tests/test_cache.py
@@ -1,15 +1,17 @@
 from __future__ import unicode_literals
+
 import os
+
+from lingpy import cache
 from lingpy.tests.util_testing import WithTempDir
 
 
 class TestCache(WithTempDir):
     def test_cache(self):
-        from lingpy import cache
-
         d = {'a': 123}
         filename = 'lingpy_test.CSV'
         cache.dump(d, filename)
+
         self.assertTrue(cache.path(filename).exists())
         self.assertEqual(cache.load(filename), d)
         os.remove(str(cache.path(filename)))
