10fd06bdbcbb635ab762726a6fbbc33ee3cfae07
Gereon Kaiping <g.a.kaiping@hum.leidenuniv.nl>
2018-07-25 17:25:21 +0200

    Add CLDF wordlist output test

--- lingpy/tests/convert/test_cldf_methods.py
+++ lingpy/tests/convert/test_cldf_methods.py
@@ -2,10 +2,11 @@ from unittest import TestCase
 
 from lingpy.basic.wordlist import Wordlist
 from lingpy.tests.util import test_data
+from lingpy.tests.util_testing import WithTempDir
 
 
 class Tests(TestCase):
-    def test_from_cldf(self):
+    def test_load_from_cldf_metadata(self):
         wl = Wordlist.from_cldf(
             test_data('cldf/test-metadata.json'),
             col="Language_ID".lower(),
@@ -16,3 +17,11 @@ class Tests(TestCase):
         assert wl.entries[0] == 'alignment'
         assert wl.cols[0] == 'anuta'.lower()
         assert wl.cols[28] == 'wallisian'
+
+class CLDFWordlistWriteTest(WithTempDir):
+    def test_load_cldf_and_write(self):
+        wl = Wordlist.from_cldf(
+            test_data('cldf/test-metadata.json'),
+            col="Language_ID".lower(),
+            row="Parameter_ID".lower())
+        wl.output('tsv', filename=str(self.tmp_path('lingpycldf')))
